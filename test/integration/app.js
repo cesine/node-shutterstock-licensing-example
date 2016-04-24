4a6c523ad79ff8f33f4b05afc353aae8b0fb074d
cesine <cesine@yahoo.com>
2016-04-24 16:12:49 -0400

    integration test

--- test/integration/app.js
+++ test/integration/app.js
@@ -1,9 +1,25 @@
 var expect = require('chai').expect;
+var supertest = require('supertest');
 
 var app = require('./../../');
 
 describe('app', function() {
   it('should load', function() {
     expect(app).to.be.a('function');
+  });
+
+  it('should handle routes which are not found', function(done) {
+    supertest(app)
+      .get('/notexistant')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect('Content-Length', '2873')
+      .expect(404)
+      .end(function(err, res) {
+        if (err) throw err;
+
+        expect(res.body).to.deep.equal({});
+
+        done();
+      });
   });
 });
---
+++ test/integration/user.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var supertest = require('supertest');
+
+var app = require('./../../');
+
+describe('/v1/users', function() {
+  it('should load', function(done) {
+    supertest(app)
+      .get('/v1/users')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+
+        expect(res.body).to.deep.equal({
+          username: 'anonymouse'
+        });
+
+        done();
+      });
+  });
+});
